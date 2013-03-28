46597a7ba210cc3d353f07b52a7dce1744186505
Matt Sroufe <mattsroufe@gmail.com>
2013-03-29 09:51:05 +1300

    add manufacturers

--- parser.rb
+++ parser.rb
@@ -3,14 +3,33 @@ require 'open-uri'
 
 @doc = Nokogiri::HTML(open("http://ecan.govt.nz/services/online-services/pages/authorised-solid-fuel-burners.aspx"))
 
-def path(num)
-  @doc.xpath("//span[@id='ctl00_PlaceHolderContentWrapper_PlaceHolderMain_editmodepanel2_UserControlFieldControl2_ctl00_lvBurner_ctrl#{num}_ApplianceName']")
+def path(num, field)
+  @doc.xpath("//span[@id='ctl00_PlaceHolderContentWrapper_PlaceHolderMain_editmodepanel2_UserControlFieldControl2_ctl00_lvBurner_ctrl#{num}_#{field}']")
 end
 
-woodburners = Array.new
+manufacturers = Array.new
+models = Array.new
 i = 0
-until path(i).empty?
-	woodburners[i] = path(i).text
+until path(i, "ApplianceName").empty?
+	models[i] = path(i, "ApplianceName").text
+	unless path(i, "ClientName").empty?
+		manufacturers[i] = path(i, "ClientName").text
+	end
   i += 1
 end
-puts "#{woodburners.count} woodburners added to array."
+
+puts "#{manufacturers.count} manufacturers added to array."
+puts "#{models.count} woodburners added to array."
+
+puts manufacturers.compact.index
+
+manufacturers.each do |num|
+  unless num == nil
+  	puts "#{manufacturers.index(num)}: #{num}"
+  end
+end
+
+#i = 0
+#until path(i, "ApplianceName").empty?
+#	if manufacturers[i]
+#end
